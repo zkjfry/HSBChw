cc320ce885e5e5b6056e99746dd3d7a3c433af34
zkjfry <jeffreychen0826@gmail.com>
2024-11-29 23:10:09 +0800

    update tests

--- admin/src/test/java/com/hsbc/admin/TestIncidentService.java
+++ admin/src/test/java/com/hsbc/admin/TestIncidentService.java
@@ -1,9 +1,12 @@
 package com.hsbc.admin;
 
+import com.hsbc.admin.entity.IncidentEntity;
 import com.hsbc.admin.service.IncidentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class TestIncidentService {
@@ -14,5 +17,49 @@ public class TestIncidentService {
     public void test() {
         long count = incidentService.count();
         System.out.println(count);
+    }
+
+    @Test
+    public void listTest() {
+        List<IncidentEntity> lst = incidentService.getAllIncidents();
+        for (IncidentEntity entity : lst) {
+            System.out.println(entity.getDescription());
+        }
+    }
+
+    @Test
+    public void addTest() {
+        IncidentEntity incidentEntity = new IncidentEntity();
+        incidentEntity.setDescription("testingContent");
+        incidentEntity.setType(1);
+        incidentEntity.setName("test1");
+        if (incidentService.save(incidentEntity)) {
+            System.out.println(incidentEntity.getId());
+        } else {
+            System.out.println("failed");
+        }
+    }
+
+    @Test
+    public void updateTest() {
+        IncidentEntity incidentEntity = new IncidentEntity();
+        incidentEntity.setId(1L);
+        incidentEntity.setDescription("testing change of content");
+        incidentEntity.setType(0);
+        incidentEntity.setName("test2");
+        if (incidentService.save(incidentEntity)) {
+            System.out.println(incidentEntity.getDescription());
+        } else {
+            System.out.println("failed");
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        if (incidentService.removeById(1L)) {
+            System.out.println("success");
+        } else {
+            System.out.println("failed");
+        }
     }
 }
